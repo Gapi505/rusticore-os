64bfafe6796d48cfdcb061458afa57f2c9fca4d1
Gašper Sodja <sodjagas@gmail.com>
2024-03-20 18:39:59 +0100

    tests

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -1,3 +1,4 @@
+
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
@@ -7,26 +8,20 @@
 use core::panic::PanicInfo;
 use rusticore_os::println;
 
-#[no_mangle]
+#[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
-
-    #[cfg(test)]
     test_main();
 
     loop {}
 }
 
-/// This function is called on panic.
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
-}
 
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     rusticore_os::test_panic_handler(info)
+}
+
+#[test_case]
+fn test_println(){
+    println!("test_println output")
 }
